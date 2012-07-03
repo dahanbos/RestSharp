1550e69a44eaa9a748530c6c10233b5a77350c6d
Sergey Maskalik <mercury2269@gmail.com>
2012-07-02 23:45:11 -0700

    added new authenticator

---
+++ RestSharp/Authenticators/OAuth2LeggedAuthenticator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RestSharp.Authenticators
+{
+    public class OAuth2LeggedAuthenticator : IAuthenticator
+    {
+        private string _consumerKey;
+        private string _consumerSecret;
+
+        public OAuth2LeggedAuthenticator(string consumerKey, string consumerSecret)
+        {
+            _consumerKey = consumerKey;
+            _consumerSecret = consumerSecret;
+        }
+
+        public void Authenticate(IRestClient client, IRestRequest request)
+        {
+            var uri = new Uri(new Uri(client.BaseUrl), request.Resource);
+            throw new System.NotImplementedException();
+        }
+    }
+}
